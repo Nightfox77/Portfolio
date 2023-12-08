7e0bc3df33ae36fcbfbc7151a7b6f3d360fd01b5
Michael Bäuml <102950224+Nightfox77@users.noreply.github.com>
2023-12-08 11:31:15 +0100

    1

--- Js/home.js
+++ Js/home.js
@@ -1,73 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Select all elements with the class 'neon-text'
-    const neonTexts = document.querySelectorAll('.menu .neon-text');
-  
-    // Function to create the flickering effect for a single neon text element
-    function flickerText(neonText) {
-      // Get the initial text content and clear the container
-      const textContent = neonText.textContent.trim();
-      neonText.innerHTML = '';
-  
-      // Wrap each letter in a span element and append to the container
-      textContent.split('').forEach(char => {
-        const span = document.createElement('span');
-        span.textContent = char;
-        neonText.appendChild(span);
-      });
-  
-      // Select all the span elements (individual letters)
-      const letters = neonText.querySelectorAll('span');
-  
-      // Function to create the flickering effect for a single neon text element
-      function flickerLetters() {
-        // Choose a random index for a letter to flicker
-        const randomIndex = Math.floor(Math.random() * letters.length);
-  
-        // Apply opacity to each letter based on the random index
-        letters.forEach((letter, index) => {
-          const opacity = index === randomIndex ? (Math.random() > 0.1 ? 0.2 : 0.8) : 1;
-          letter.style.opacity = opacity;
-        });
-  
-        // Reset opacity for all letters after a short delay
-        setTimeout(() => {
-          letters.forEach(letter => {
-            letter.style.opacity = 1;
-          });
-  
-          // Schedule the next iteration of the flickering effect
-          setTimeout(() => {
-            requestAnimationFrame(flickerLetters);
-          }, 3000); // Adjust the delay (in milliseconds) for the pause between letters
-        }, 60); // Adjust the delay (in milliseconds) for the flickering speed
-      }
-  
-      // Start the flickering animation loop for each neon text element
-      flickerLetters();
-    }
-  
-    // Introduce a delay for each neon text element before starting its flickering animation
-    neonTexts.forEach((neonText, index) => {
-      setTimeout(() => {
-        flickerText(neonText);
-      }, index * 1000); // Adjust the delay (in milliseconds) between each neon text element
-    });
-  });
-  
 const burger = document.querySelector("#burgerIcon");
 const overlay = document.querySelector(".overlay");
 const menu = document.querySelector(".menu");
 const closeIcon = document.querySelector("#xIcon");
 const neon = document.querySelectorAll(".menu .neon-text");
-const logo =  document.querySelectorAll(".logo")
 const topContainer = document.querySelector(".top");
 
-function handleScreenSize() {
-if (window.matchMedia('(max-width: 1000px)').matches) {
-  menu.style.display = "none";
-  neon.forEach(function(element) {
-    element.addEventListener('click', hideMenu);
+neon.forEach(function(element) {
+  element.addEventListener('click', checkScreen);
 });
+
+function checkScreen() {
+  if (window.matchMedia('(max-width: 1000px)').matches) {
+    hideMenu();
+  } else {
+    menu.style.display = "flex";
+  }
+}
 
 function showMenu() {
   topContainer.style.flexDirection = "column";
@@ -77,6 +25,7 @@ function showMenu() {
   burger.style.display = "none";
   closeIcon.style.display = "block";
 }
+
 function hideMenu() {
   topContainer.style.flexDirection = "row";
   overlay.style.transform = "translateX(-100%)";
@@ -84,37 +33,21 @@ function hideMenu() {
   burger.style.display = "block";
   closeIcon.style.display = "none";
 }
+
 burger.onclick = showMenu;
 closeIcon.onclick = hideMenu;
 
-
-} else {
-  menu.style.display = "flex";
-
-}}
-
-window.addEventListener('scroll', function hideReflection() {
-if (window.matchMedia('(min-width: 1000px)').matches) {
-  
-
-
-
-if (window.scrollY > 0) {
-  neon.forEach(function(element) {
-  element.classList.add('no');
-})
-} else {
-  neon.forEach(function(element) {
-  element.classList.remove('no');
-  })
+function handleScreenSize() {
+  if (window.matchMedia('(min-width: 1000px)').matches) {
+    menu.style.display = "flex";
+  } else {
+    menu.style.display = "none";
+  }
 }
-}
-})
-
-handleScreenSize();
-
-// Check for screen size on resize
 window.addEventListener('resize', handleScreenSize);
+
+
+
 
 
 const home = document.querySelector("#homeButton");
@@ -131,3 +64,49 @@ home.onclick = scrollToTop;
 up.onclick = scrollToTop;
 
 
+window.addEventListener('scroll', function hideReflection() {
+  if (window.matchMedia('(min-width: 1000px)').matches) {
+    
+  
+  
+  
+  if (window.scrollY > 0) {
+    neon.forEach(function(element) {
+    element.classList.add('no');
+  })
+  } else {
+    neon.forEach(function(element) {
+    element.classList.remove('no');
+    })
+  }
+  }
+  })
+
+  function flickerAnimation(element) {
+    function flicker() {
+      element.style.opacity = 0;
+      setTimeout(() => { element.style.opacity = 1; }, Math.random() * 60); // Random delay for reappearing (up to 100ms)
+      setTimeout(flicker, Math.random() * 5000); // Random delay before next flicker (up to 300ms)
+    }
+
+    flicker();
+  }
+
+  function randomFlicker() {
+    const buttons = document.querySelectorAll('.neon-text');
+    const randomIndex = Math.floor(Math.random() * buttons.length);
+    buttons.forEach((button, index) => {
+      if (index === randomIndex) {
+        flickerAnimation(button);
+      }
+    });
+  }
+
+  // Initial random flicker
+  randomFlicker();
+
+  // Set interval to change flicker every few seconds for one button at a time
+  setInterval(randomFlicker, 7000); // Change every 5 seconds
+  
+  
+  
