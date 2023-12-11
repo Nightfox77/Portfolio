eb6a5dc3dace7a9809b69fae39cf849f6230cfa0
Michael Bäuml <102950224+Nightfox77@users.noreply.github.com>
2023-12-11 17:22:48 +0100

    1

--- Js/home.js
+++ Js/home.js
@@ -49,7 +49,6 @@ window.addEventListener('resize', handleScreenSize);
 
 
 
-
 /* scroll back to top function for home and up button */
 const home = document.querySelector("#homeButton");
 const up = document.querySelector("#upButton")
@@ -65,20 +64,20 @@ home.onclick = scrollToTop;
 up.onclick = scrollToTop;
 
 /* hide reflection of buttons in top as soon as scrolling starts */
-window.addEventListener('scroll', function hideReflection() {
-  if (window.matchMedia('(min-width: 1000px)').matches) {
+// window.addEventListener('scroll', function hideReflection() {
+//   if (window.matchMedia('(min-width: 1000px)').matches) {
     
-    if (window.scrollY > 0) {
-    neon.forEach(function(element) {
-    element.classList.add('no');
-  })
-  } else {
-    neon.forEach(function(element) {
-    element.classList.remove('no');
-    })
-  }
-  }
-  })
+//     if (window.scrollY > 0) {
+//     neon.forEach(function(element) {
+//     element.classList.add('no');
+//   })
+//   } else {
+//     neon.forEach(function(element) {
+//     element.classList.remove('no');
+//     })
+//   }
+//   }
+//   })
 /* flicker animation for the buttons */
   function flickerAnimation(element) {
     function flicker() {
