c3f2a39f250c23f595e89726f27d893484a1729e
Michael Bäuml <102950224+Nightfox77@users.noreply.github.com>
2023-12-13 17:22:19 +0100

    header width vw

--- Js/home.js
+++ Js/home.js
@@ -98,18 +98,3 @@ window.addEventListener('scroll', function hideReflection() {
  
   randomFlicker();
 
-/* slidein effect */  
-document.addEventListener('DOMContentLoaded', function() {
-const observer = new IntersectionObserver((entries) =>{
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-    } else {
-      entry.target.classList.remove('show');
-    }
-  })
-})
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));  
-})
